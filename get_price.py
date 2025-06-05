727894c2c60ee2789f84e87c4190bb7378d926bc
Tomorrow0912 <thomasmorrow574@gmail.com>
2025-06-05 16:55:12 +1000

    V1 , tested pulling data from yfinance

---
+++ get_price.py
@@ -0,0 +1,6 @@
+import yfinance as yf
+symbol = input("Enter ticker: ")
+Ticker = yf.Ticker(symbol)
+history = Ticker.history(period="1d", interval="15m")
+latest_close = history["Close"].iloc[-1]
+print(f"Latest close price: {latest_close:.2f}")
