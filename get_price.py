211b1c4c29cbca4c2fb854d26d76208b8e11a6e7
Thomas Morrow <thomasmorrow574@gmail.com>
2025-06-06 21:55:27 +1000

    Update config.py

--- get_price.py
+++ get_price.py
@@ -4,3 +4,4 @@ Ticker = yf.Ticker(symbol)
 history = Ticker.history(period="1d", interval="15m")
 latest_close = history["Close"].iloc[-1]
 print(f"Latest close price: {latest_close:.2f}")
+#testing
